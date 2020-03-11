cecc3f8b2095ae2b95646da2d3a87bc5d984fa1a
Johan N <info@superdb.cc>
2020-03-11 09:44:32 +0100

    changes to how games are presentes. CSS

--- resources/views/game/game.blade.php
+++ resources/views/game/game.blade.php
@@ -3,27 +3,63 @@
 @section('title', $game->title)
 
 @section('content')
-    <h1>{{ $game->console }}: {{ $game->title }}</h1>
+<h1>{{ $game->console }}: {{ $game->title }}</h1>
     <span style="text-decoration: underline solid">Tillagd:</span> {{ $game->created_at->diffForhumans() }} ({{ $game->created_at }})<br>
     <span style="text-decoration: underline solid">Senaste uppdaterad:</span> {{ $game->updated_at->diffForHumans() }} ({{ $game->updated_at }})<br>
-    <br>
 
-    <span style="text-decoration: underline solid">Superdb ID:</span> #{{ $game->id }}<br>
-    <span style="text-decoration: underline solid">import:</span> {{ $game->import }}<br>
-    <span style="text-decoration: underline solid">genre:</span> {{ $game->genre }}<br>
-    <span style="text-decoration: underline solid">modes:</span> {{ $game->modes }}<br>
-    <span style="text-decoration: underline solid">publisher:</span> {{ $game->publisher }}<br>
-    <span style="text-decoration: underline solid">developer:</span> {{ $game->developer }}<br>
-    <img src="{{ asset('images/se.png') }}" class="image"><span style="text-decoration: underline solid">sweden_release:</span> {{ $game->sweden_release }}<br>
-       <img src="{{ asset('images/eu.png') }}" class="image"> <span style="text-decoration: underline solid">europe_release:</span> {{ $game->europe_release }}<br>
-       <img src="{{ asset('images/jp.png') }}" class="image"> <span style="text-decoration: underline solid">japan_release:</span> {{ $game->japan_release }}<br>
-       <img src="{{ asset('images/us.png') }}" class="image"> <span style="text-decoration: underline solid">usa_release:</span> {{ $game->usa_release }}<br>
-    <span style="text-decoration: underline solid">save:</span> {{ $game->save }}<br>
-       @if($game->console == 'snes')
-        <span style="text-decoration: underline solid">snes_central:</span> <a href="{{ $game->snes_central ?? 'http://www.google.com' }}">LINK</a><br>
-       @endif
-    <span style="text-decoration: underline solid">wikipedia_url:</span> <a href="{{ $game->wikipedia_url ?? "https://www.google.com/search?q=" . $game->title . "+wikipedia+" .  $game->console }}">LINK</a><br>
-    <span style="text-decoration: underline solid">releaser:</span> {{ $game->releases->count() }}<br>
-    <span style="text-decoration: underline solid">description:</span> {{ $game->description }}<br>
+<ul class="list">
+  <li class="list-element">
+    <span style="">Superdb ID:</span> #{{ $game->id }}
+  </li>
+  <li class="list-element">
+    <span style="">import:</span> {{ $game->import }}
+  </li>
+  <li class="list-element">
+    <span style="">publisher:</span> {{ $game->publisher }}
+  </li>
+  <li class="list-element">
+    <span style="">developer:</span> {{ $game->developer }}
+  </li>
+  <li class="list-element">
+    <img src="{{ asset('images/se.png') }}" class="image"> <span style="">sweden_release:</span> {{ $game->sweden_release }}
+  </li>
+  <li class="list-element">
+   <img src="{{ asset('images/eu.png') }}" class="image"> <span style="">europe_release:</span> {{ $game->europe_release }}
+ </li>
+ <li class="list-element">
+   <img src="{{ asset('images/jp.png') }}" class="image"> <span style="">japan_release:</span> {{ $game->japan_release }}
+ </li>
+ <li class="list-element">
+   <img src="{{ asset('images/us.png') }}" class="image"> <span style="">usa_release:</span> {{ $game->usa_release }}
+ </li>
+  <li class="list-element">
+    <span style="">genre:</span> {{ $game->genre }}
+  </li>
+  <li class="list-element">
+    <span style="">modes:</span> {{ $game->modes }}
+  </li>
+ <li class="list-element">
+  <span style="">save:</span> {{ $game->save }}
+</li>
+ @if($game->console == 'snes')
+ <li class="list-element">
+  <span style="">snes_central:</span> <a href="{{ $game->snes_central ?? 'http://www.google.com' }}">LINK</a>
+</li>
+ @endif
+ <li class="list-element">
+  <span style="">wikipedia_url:</span> <a href="{{ $game->wikipedia_url ?? "https://www.google.com/search?q=" . $game->title . "+wikipedia+" .  $game->console }}">LINK</a>
+</li>
+<li class="list-element">
+  <span style="">releaser:</span> {{ $game->releases->count() }}
+</li>
+<li class="list-element">
+  <span style="">description:</span> {{ $game->description }}  
+</li>
+</ul>
+
+
+@can('update')
+  edit
+@endcan
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,37 +3,53 @@
 @section('title', 'välkommen')
 
 @section('content')
-    <h1>superdb.cc 🍻</h1>
-    <h3>Databas över Nintendo titlar till NES, SNES, N64, GC, GBA och GBC</h3>
-   
-    <div class="toast toast-primary">
+<h1>superdb.cc 🍻</h1>
+<h3>Databas över Nintendo titlar till NES, SNES, N64, GC, GBA och GBC</h3>
+
+<div class="toast toast-primary">
     (Observera att databasen gör ett försök till att enbart innehålla titlar utgivna i Norden.
     Dessvärre är N64, GC, GBA och GBC inte långt gågna i den processen.)
-    </div>
+</div>
 
-    <ul>
-        <li><a href="{{ route('nes') }}">NES</a>, {{ $nes_count }} titlar</li>
-        <li><a href="{{ route('snes') }}">SNES</a>, {{ $snes_count }} titlar</li>
-        <li><a href="{{ route('n64') }}">N64</a>, {{ $n64_count }} titlar</li>
-        <li><a href="{{ route('ngc') }}">NGC</a>, {{ $ngc_count }} titlar</li>
-        <li><a href="{{ route('gba') }}">GBA</a>, {{ $gba_count }} titlar</li>
-        <li><a href="{{ route('gbc') }}">GBC</a>, {{ $gbc_count }} titlar</li>
-    </ul>
-    
-    <p>
+<ul>
+    <li>
+        <a href="{{ route('nes') }}">NES</a>, {{ $nes_count }} titlar
+    </li>
+    <li>
+        <a href="{{ route('snes') }}">SNES</a>, {{ $snes_count }} titlar
+    </li>
+    <li>
+        <a href="{{ route('n64') }}">N64</a>, {{ $n64_count }} titlar
+    </li>
+    <li>
+        <a href="{{ route('ngc') }}">NGC</a>, {{ $ngc_count }} titlar
+    </li>
+    <li>
+        <a href="{{ route('gba') }}">GBA</a>, {{ $gba_count }} titlar
+    </li>
+    <li>
+        <a href="{{ route('gbc') }}">GBC</a>, {{ $gbc_count }} titlar
+    </li>
+</ul>
+
+<p>
     Antal rader: 2010 <br>
     Genomsnittlig radlängd: ~199 <br>
     Dumpad databasstorlek: ~6,2M <br>
-    </p>
+</p>
 <hr>
 <div>
-    <p><b>2020-03-07- 11:31:44</b></p>
-    <p>Efter mycket mödosamt arbete så börjar databasen ta form! Det stora arbetet som har varit är datum, att ändra datumen till ISO 8601 med manuell handpåläggning men också automatsikt med divere pythonskript. Det arbete som fortskrider kontinuerligt är bland annat att lägga in releaser, uppdatera utgivare, genre och modes. Ja, ni fattar.</p>
-    <p>Är du också entusiast? Maila mig!</p>
+    <p style="margin-bottom: 0px"><b>2020-03-11 06:09:49</b></p>
+    <p style="margin-top: 0px;">Inne i en uppdateringsperiod just nu! Det senaste är små kosmetiska ändringar. Dessvärre får jag inte till det med navigationsmenyn när sidan visas på mindre skärmar, exempelvis smartphones. Den flyttas från vänster sida och lägger sig i nederkant. Men det går att navigera trots de felen.</p>
+    <p>Jag håller också på med att "koppla samman" spel mellan olika konsoler. Kan vara intressant att se om samma spel, eller i varje fall titel, finns på någon annan konsol. //JNI</p>
+
+    <p style="margin-bottom: 0px"><b>2020-03-07 11:31:44</b></p>
+    <p style="margin-top: 0px;">Efter mycket mödosamt arbete så börjar databasen ta form! Det stora arbetet som har varit är datum, att ändra datumen till ISO 8601 med manuell handpåläggning men också automatsikt med divere pythonskript. Det arbete som fortskrider kontinuerligt är bland annat att lägga in releaser, uppdatera utgivare, genre och modes. Ja, ni fattar.</p>
+    <p>Är du också entusiast? Maila mig! //JNI</p>
 </div>
 <hr>
 
-    <p>
+<p>
     <a href="mailto:info@superdb.cc">info@superdb.cc</a>
-    </p>
+</p>
 @endsection
