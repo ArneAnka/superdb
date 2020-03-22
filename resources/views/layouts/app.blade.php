6cb5212b597eeb2c7fb5fa140373c2a8d23820e0
Johan N <info@superdb.cc>
2020-03-22 07:00:45 +0100

    Added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,10 @@
 <main>
     @yield('content')
 
-    <div style="margin-top: 5em;">
-        <script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'I2I11DVRB');kofiwidget2.draw();</script>
-    </div>
+    @include('layouts.partials._footer')
 </main>
 
+    <!-- Extra scripts -->
+    @yield('scripts')
 </body>
 </html>
