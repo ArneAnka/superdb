d3589f45107048972962783c4da5f80355208414
Johan N <info@superdb.cc>
2020-05-05 10:25:21 +0200

    la till i ändringloggen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,17 +39,20 @@
 </p>
 <hr>
 <div>
-    <p style="margin-bottom: 0px"><b>2020-03-25 06:01:32</b></p>
+    <p style="margin-bottom: 0px"><b>Tis  5 Maj 2020 10:21:40</b></p>
+    <p style="margin-top: 0px;">Mer information om andra spel visas nu när du visar ett specefikt spel. //JNI</p>
+<hr>
+    <p style="margin-bottom: 0px"><b>Ons 25 Mar 2020 06:01:32</b></p>
     <p style="margin-top: 0px;">Ändringshistorik tillagd //JNI</p>
 <hr>
-    <p style="margin-bottom: 0px"><b>2020-03-23 06:04:26</b></p>
+    <p style="margin-bottom: 0px"><b>Mån 23 Mar 2020 06:04:26</b></p>
     <p style="margin-top: 0px;">Arbetar just nu med att alla SNES-spel skall få en "beskrivnig" (description). Tar tid! //JNI</p>
 <hr>
-    <p style="margin-bottom: 0px"><b>2020-03-11 06:09:49</b></p>
+    <p style="margin-bottom: 0px"><b>Ons 11 Mar 2020 06:09:49</b></p>
     <p style="margin-top: 0px;">Inne i en uppdateringsperiod just nu! Det senaste är små kosmetiska ändringar. Dessvärre får jag inte till det med navigationsmenyn när sidan visas på mindre skärmar, exempelvis smartphones. Den flyttas från vänster sida och lägger sig i nederkant. Men det går att navigera trots de felen.</p>
     <p>Jag håller också på med att "koppla samman" spel mellan olika konsoler. Kan vara intressant att se om samma spel, eller i varje fall titel, finns på någon annan konsol. //JNI</p>
 <hr>
-    <p style="margin-bottom: 0px"><b>2020-03-07 11:31:44</b></p>
+    <p style="margin-bottom: 0px"><b>Lör 7 Mar 2020 11:31:44</b></p>
     <p style="margin-top: 0px;">Efter mycket mödosamt arbete så börjar databasen ta form! Det stora arbetet som har varit är datum, att ändra datumen till ISO 8601 med manuell handpåläggning men också automatsikt med divere pythonskript. Det arbete som fortskrider kontinuerligt är bland annat att lägga in releaser, uppdatera utgivare, genre och modes. Ja, ni fattar.</p>
     <p>Är du också entusiast? Maila mig! //JNI</p>
 </div>
