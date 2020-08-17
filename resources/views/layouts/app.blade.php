f90412f42808b1f560abd172d8303a5a1de28fee
Johan N <info@superdb.cc>
2020-08-17 17:44:55 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <title>SuperDB.cc - @yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Icon -->
-    <link rel="icon" href="{!! asset('images/gcm_ico.ico') !!}"/>
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" />
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
