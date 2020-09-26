d07a1c5f0796dfff61b8bbb2fcb801687a9ffe00
Johan N <info@superdb.cc>
2020-09-26 17:01:54 +0200

    Nu visas poängen

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -6,8 +6,11 @@
 <div class="container mx-auto px-4">
     <div class="space-y-5">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">{{ $user->name }}</h2>
+        <p>{{ $user->points()->shorthand() }} poäng.</p>
         @can('update', $user)
+        <div>
             <a class="underline" href="{{ route('user.edit', $user) }}">Edit profile</a>
+        </div>
         @endcan
         <div>
         </div>
