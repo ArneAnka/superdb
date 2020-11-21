7e9a4c761a800466c2577ae1f5445b40cd4a84f4
Johan N <info@superdb.cc>
2020-11-21 11:24:03 +0100

    ändringar

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -19,27 +19,28 @@
 
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Senaste {{ count($user->comments) }} kommentarena</h2>
         <div>
-        <ul class="list-disc">
-            @foreach($user->comments as $comment)
-                @include('user.comments.partials._' . strtolower(class_basename($comment->commentable_type)))
-            @endforeach
-        </ul>
+            <ul class="list-disc">
+                @foreach($user->comments as $comment)
+                    @include('user.comments.partials._' . strtolower(class_basename($comment->commentable_type)))
+                @endforeach
+            </ul>
+        </div>
 
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Senaste {{ count($user->history) }} redigeringarna</h2>
         <div>
-        <ul class="list-disc">
-            @forelse($user->history as $history)
-                @include('user.history.partials._' . strtolower(class_basename($history->historyable_type)))
-            @empty
-                <div class="flex items-center">
-                    <div>
-                        &nbsp;Inga ändringar gjorda av denna användaren.
+            <ul class="list-disc text-gray-500">
+                @forelse($user->history as $history)
+                    @include('user.history.partials._' . strtolower(class_basename($history->historyable_type)))
+                @empty
+                    <div class="flex items-center">
+                        <div>
+                            &nbsp;Inga ändringar gjorda av denna användaren.
+                        </div>
                     </div>
-                </div>
-            @endforelse
-        </ul>
-
+                @endforelse
+            </ul>
         </div>
-    </div> <!-- end container -->
-</div>
+
+    </div>
+</div> <!-- end container -->
 @endsection
