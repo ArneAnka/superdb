c3b1afb7ae31f35d038e8cb40c93239da4b5bfe0
Johan N <info@superdb.cc>
2020-12-10 13:01:33 +0100

    små layouts

--- resources/views/developer.blade.php
+++ resources/views/developer.blade.php
@@ -10,12 +10,12 @@
         </h2>
         <p>{{ $developer->description }}</p>
 
-        <ul class="list-disc">
+        <ul class="list-disc list-inside">
             @foreach($grouped as $key => $games)
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
                     {{ $key }}
                 </h2>
-                <ul class="list-disc mb-4">
+                <ul class="list-disc list-inside mb-4">
                     @foreach($games as $game)
                         <li>
                             <a class="underline" href="{{ route('game.show', $game) }}">{{ $game->title }}</a>
