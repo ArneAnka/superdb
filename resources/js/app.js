7120bd15a629c3faa21fd468ecb7c0ea70b7f21e
Johan N <info@superdb.cc>
2021-01-11 04:21:40 +0100

    TailwindCSS update with PurgeCSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 // Vue.component('search-component', require('./components/GameSearchComponent.vue').default);
-Vue.component('user-dropdown', require('./components/UserDropDownComponent.vue').default);
+// Vue.component('user-dropdown', require('./components/UserDropDownComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,23 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss')
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
  */
+mix.js('resources/js/app.js', 'public/js');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/main.css', 'public/css', [
-      require('tailwindcss'),
-    ]);
+mix.sass('./resources/css/main.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
+    .purgeCss();
+
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/main.css', 'public/css', [
+//       require('tailwindcss'),
+// ]);
