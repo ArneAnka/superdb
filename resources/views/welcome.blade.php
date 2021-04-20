2b60d46b141cc326392bd63ef812ca168bc80963
JOHAN NILSSON <ohan.pa.vattumansgatan@gmail.com>
2021-04-20 18:47:34 +0200

    Fick det inte att fungera

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">#{{ $post->id }} {{ $post->topic }}</h2>    
             </div>
             <div class="ml-4">
-                @markdown($post->body)
+                {{ $post->body }}
             </div>
            <div class="ml-4 mt-5 ">
                <div class="flex items-center">
